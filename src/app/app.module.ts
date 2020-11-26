6fde0bfccd3f4ff77743dfaaf94fe2c81f5a2c89
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2020-11-26 16:51:26 -0300

    Layout básico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { FooterComponent } from './layouts/basic-layout/footer/footer.component';
+import { MainComponent } from './layouts/basic-layout/main/main.component';
+import { HeaderComponent } from './layouts/basic-layout/header/header.component';
+import { BasicLayoutComponent } from './layouts/basic-layout/basic-layout.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicLayoutComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layouts/basic-layout/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  currentDate: Date;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.currentDate = new Date();
+  }
+
+}
