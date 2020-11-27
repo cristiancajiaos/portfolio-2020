1774915014ca7c5fc920f1eb4c330be1cf2ad8ff
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2020-11-27 17:28:21 -0300

    Composición de Módulos básica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { BasicLayoutComponent } from './layouts/basic-layout/basic-layout.compon
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PerfectScrollbarModule, PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,8 +26,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FontAwesomeModule,
+    PerfectScrollbarModule
   ],
-  providers: [],
+  providers: [{
+    provide: PERFECT_SCROLLBAR_CONFIG,
+    useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/interfaces/social-network.ts
@@ -0,0 +1,5 @@
+export interface SocialNetwork {
+  name: string;
+  icon: string;
+  url: string;
+}
