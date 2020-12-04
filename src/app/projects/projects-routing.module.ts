eed57d1ada540ade094d713cd1ab7ea824d8f48b
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2020-12-04 18:45:16 -0300

    Agregado ruteo por ID al módulo Projects

--- src/app/projects/projects-routing.module.ts
+++ src/app/projects/projects-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectComponent } from './project/project.component';
 
 import { ProjectsComponent } from './projects.component';
 
-const routes: Routes = [{ path: '', component: ProjectsComponent }];
+const routes: Routes = [
+  { path: '', component: ProjectsComponent },
+  { path: ':id', component: ProjectComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
