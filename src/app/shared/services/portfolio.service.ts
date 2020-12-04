4870b9a2cd7dc8782bfe85fc473cf69d035f1959
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2020-12-04 18:44:45 -0300

    Búsqueda por ID agregada al servicio Portfolio

--- src/app/shared/services/portfolio.service.ts
+++ src/app/shared/services/portfolio.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PortfolioElement } from '../interfaces/portfolio-element';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,9 @@ export class PortfolioService {
 
   getAllPortfolioElements(): Observable<PortfolioElement[]> {
     return this.http.get<PortfolioElement[]>('assets/data/portfolio.json');
+  }
+
+  getPortfolioElementById(id: number): Observable<PortfolioElement> {
+    return this.getAllPortfolioElements().pipe(map(elements => elements.filter(element => element.id === id)[0]));
   }
 }
