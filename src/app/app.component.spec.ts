f967e886cefc7ef6b7c85075dc97ec823482e7c1
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2021-11-10 18:48:37 -0300

    Cambio de nombre portfolio a portfolio-2020

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio'`, () => {
+  it(`should have as title 'portfolio-2020'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio');
+    expect(app.title).toEqual('portfolio-2020');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('portfolio app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('portfolio-2020 app is running!');
   });
 });
